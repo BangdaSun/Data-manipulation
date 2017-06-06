0222063bce62173b57f08ee7615631e6794ae796
Bangda Sun <bangdasun94@gmail.com>
2017-06-05 22:02:41 -0400

    Create date-time.R

---
+++ R/date-time.R
@@ -0,0 +1,63 @@
+################################
+### Chapter 4. Date and Time ###
+################################
+
+### 4.1 as.Date (Date only)
+#   format = ...
+#   default: year with 4 digits - month - day with - or /
+
+as.Date("2017-06-05")
+as.Date("6/5/2017", format = "%m/%d/%Y")
+as.Date("June 5, 2017", format = "%B %d, %Y")
+as.Date("5JUN17", format = "%d%b%y")
+
+#   date object is number of days from 1970/01/01
+as.numeric(as.Date("2017-06-05"))
+as.numeric(as.Date("2014-12-10"))
+
+day = 17322
+class(day) = "Date"
+day
+
+weekdays(day)
+
+### 4.2 chron package
+#   if the data has date and time, we need split them first
+library(chron)
+dtimes = "2017-06-05 21:28:40"
+dtparts = strsplit(dtimes, split = " ")
+dtparts
+chron(dates = dtparts[[1]][1], times = dtparts[[1]][2],
+      format = c("y-m-d", "h:m:s")) # mon, month
+
+### 4.3 POSIX class
+#  POSIXct: use second from 1970/01/01 (default in R)
+#  POSIXlt: second, minute, hour, day, month, year as a list
+#  they also work for different time zone
+
+#  valid input for POSIXct:
+#  2017/6/5
+#  2017-06-05 21:37
+#  2017/6/5 21:37:55
+dts = c("2005-10-21 18:47:22 EDT",
+        "2005-10-24 16:39:58 EDT",
+        "2005-10-28 07:30:05 EDT")
+as.POSIXct(dts)
+as.POSIXlt(dts)
+
+
+### 4.4
+
+### 4.5 Time interval
+b1 = as.Date("2017-06-05")
+b2 = as.Date("2016-09-06")
+b1 - b2
+
+#   by different time unit
+difftime(b1, b2, units = "weeks")
+difftime(b1, b2, units = "mins")
+
+### 4.6 Time series
+#   generate time index sequence
+seq(as.Date("2016-09-06"), by = "days", length = 10)
+seq(as.Date("2016-09-06"), by = "weeks", length = 20)
